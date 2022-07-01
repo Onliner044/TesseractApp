daa6735cb9de8a7461b25380fd05b2bddde2a0b1
Igor <onliner044@gmail.com>
2022-07-01 12:06:50 +0300

    corrected orthographic projection

--- Graphics/Projection.cs
+++ Graphics/Projection.cs
@@ -7,9 +7,6 @@ namespace Graphics
 {
     internal class Projection
     {
-        private float _z0;
-        private float _resolution;
-        private float _fieldOfView;
         private SizeF _halfBoundSize;
 
         public Projection(SizeF size)
@@ -19,18 +16,14 @@ namespace Graphics
 
         internal void SetBounds(SizeF size)
         {
-            _resolution = size.Height;
-            _fieldOfView = Converter.DegToRad(70.0f);
-            _z0 = (_resolution / 2.0f) / (float)Math.Tan((_fieldOfView / 2.0f) * (float)Math.PI / 180.0f);
-
             _halfBoundSize = new SizeF(size.Width / 2, size.Height / 2);
         }
 
-        internal PointF ProjectVector(Vector3 vector)
+        internal PointF OrthographicProjection(Vector3 vector)
         {
             PointF point = new PointF();
-            point.X = vector.X * _z0 / (_z0 + vector.Z) + _halfBoundSize.Width;
-            point.Y = -vector.Y * _z0 / (_z0 + vector.Z) + _halfBoundSize.Height;
+            point.X = vector.X + _halfBoundSize.Width;
+            point.Y = vector.Y + _halfBoundSize.Height;
 
             return point;
         }
